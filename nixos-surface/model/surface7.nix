c5684cca59191fe9fb4175b412432053905aaf73
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 14:19:55 +0100

    Update surface7.nix

--- nixos-surface/model/surface7.nix
+++ nixos-surface/model/surface7.nix
@@ -1,4 +1,12 @@
 { config, pkgs, ... }:
 rec {
-  
+  boot = {
+	  blacklistedKernelModules = [ "surfacepro3_button" "nouveau" ];
+	  kernelPackages = pkgs.surface_kernel;
+	  initrd = {
+	    kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "ipts" "surface_acpi" ];
+	    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+	  };
+	  kernelParams = [ "reboot=pci" ];
+  };
 }
