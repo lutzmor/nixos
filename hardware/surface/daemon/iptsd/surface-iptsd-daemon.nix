f32df18671126c25946936c1b7e5447bef3acbd5
lutzmor <Moritz.Lutz@gmail.com>
2020-11-04 16:19:53 +0100

    added service patch for iptsd

--- hardware/surface/daemon/iptsd/surface-iptsd-daemon.nix
+++ hardware/surface/daemon/iptsd/surface-iptsd-daemon.nix
@@ -9,6 +9,9 @@ in pkgs.stdenv.mkDerivation {
     rev = "e4627e8a3a1e88b452811a1414836e9433ff89e7";
     sha256 = "17x6g4s5lnzb3s5lcvr70wdsy4qxnz026g1zwacb1275kv53sl4h";
   };
+  
+  patches = [ patches/service.patch ];
+
   buildInputs = with pkgs; [ meson ninja pkgconfig cmake (import ./libinih/inih.nix)];
 
   preInstall = ''
