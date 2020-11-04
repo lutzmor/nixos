1140e08a9793114a7a1c9ee26778c93727f098d0
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 15:25:39 +0100

    Update surface.nix

--- hosts/surface/surface.nix
+++ hosts/surface/surface.nix
@@ -8,14 +8,4 @@
 	hostName = "surface";
 	interfaces.wlp0s20f3.useDHCP = true;
   };
- 
-  boot = {
-	  blacklistedKernelModules = [ "surfacepro3_button" "nouveau" ];
-	  kernelPackages = pkgs.surface_kernel;
-	  initrd = {
-	    kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "ipts" "surface_acpi" ];
-	    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
-	  };
-	  kernelParams = [ "reboot=pci" ];
-  };
 }
