3cae9cbd8917781ba63013bd911cdd9614fe24ce
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 13:27:09 +0100

    Update surface.nix

--- nixos-surface/surface.nix
+++ nixos-surface/surface.nix
@@ -7,27 +7,7 @@
   nixpkgs.overlays = [(self: super: {
 	surface_firmware = super.callPackage ./firmware/surface-firmware.nix {};
 	surface-iptsd-daemon = super.callPackage ./daemon/surface-iptsd-daemon.nix {};
-	surface_kernel = super.linuxPackages_5_9.extend( self: (ksuper: {
-	  kernel = ksuper.kernel.override {
-	    kernelPatches = [
-		{ patch = kernel/linux-surface/patches/5.9/0001-surface3-oemb.patch; name = "1"; }
-		{ patch = kernel/linux-surface/patches/5.9/0002-wifi.patch; name = "2"; }
-		{ patch = kernel/linux-surface/patches/5.9/0003-ipts.patch; name = "3"; }
-		{ patch = kernel/linux-surface/patches/5.9/0004-surface-gpe.patch; name = "4"; }
-		{ patch = kernel/linux-surface/patches/5.9/0005-surface-sam-over-hid.patch; name = "5"; }
-		{ patch = kernel/linux-surface/patches/5.9/0006-surface-sam.patch; name = "6"; }
-	    ];
-	    extraConfig = ''
-          	SERIAL_DEV_BUS y
-          	SERIAL_DEV_CTRL_TTYPORT y
-          	INPUT_SOC_BUTTON_ARRAY m
-          	SURFACE_3_POWER_OPREGION m
-          	SURFACE_3_BUTTON m
-          	SURFACE_3_POWER_OPREGION m
-          	SURFACE_PRO3_BUTTON m
-	    '';
-	  };
-	}));
+	surface_kernel = super.callPackage ./kernel/kernel_5_9.nix {};
   })];
 
   hardware = {
