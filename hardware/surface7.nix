92ca1f8b6affe3abbbe0d026b645b6325dc58fb0
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 15:06:13 +0100

    Update surface7.nix

--- hardware/surface7.nix
+++ hardware/surface7.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }
+{ config, pkgs, ... }:
 {
+  imports = [
+    ./surface/surface.nix
+  ];
+  
   boot = {
 	  blacklistedKernelModules = [ "surfacepro3_button" "nouveau" ];
-	  kernelPackages = pkgs.surface_kernel;
 	  initrd = {
 	    kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "ipts" "surface_acpi" ];
 	    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
