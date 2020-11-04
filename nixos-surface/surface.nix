d582c69203f7bb9004ef87192b7aad82d23bd5d1
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 14:19:33 +0100

    Update surface.nix

--- nixos-surface/surface.nix
+++ nixos-surface/surface.nix
@@ -14,15 +14,15 @@
   	firmware = [ pkgs.surface_firmware ];
   };
 
-  boot = {
-	blacklistedKernelModules = [ "surfacepro3_button" "nouveau" ];
-	kernelPackages = pkgs.surface_kernel;
-	initrd = {
-	  kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "ipts" "surface_acpi" ];
-	  availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
-	};
-	kernelParams = [ "reboot=pci" ];
-  };
+#  boot = {
+#	blacklistedKernelModules = [ "surfacepro3_button" "nouveau" ];
+#	kernelPackages = pkgs.surface_kernel;
+#	initrd = {
+#	  kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "ipts" "surface_acpi" ];
+#	  availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+#	};
+#	kernelParams = [ "reboot=pci" ];
+#  };
 
   services = {
 	udev.packages = [ pkgs.surface_firmware ];
