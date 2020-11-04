26ef87d80e8e8140768255a56f0299a9f3997990
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 15:08:32 +0100

    Create surface_book2.nix

---
+++ hardware/surface_book2.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./surface/surface_book.nix
+  ];
+  
+  boot = {
+	  blacklistedKernelModules = [ "surfacepro3_button" "nouveau" ];
+	  initrd = {
+	    kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "ipts" "surface_acpi" ];
+	    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+	  };
+  };
+}
