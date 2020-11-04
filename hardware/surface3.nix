afea815c4dafeac6ba19edc9eb31dfe87dc4ddb4
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 15:10:52 +0100

    Update surface3.nix

--- hardware/surface3.nix
+++ hardware/surface3.nix
@@ -1,1 +1,14 @@
-TODO
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./surface/surface.nix
+  ];
+  
+  boot = {
+	  blacklistedKernelModules = [ "nouveau" ];
+	  initrd = {
+	    kernelModules = [ "hid" "hid_sensor_hub" "i2c_hid" "hid_generic" "usbhid" "hid_multitouch" "intel_ipts" "surface_acpi" ];
+	    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+	  };
+  };
+}
